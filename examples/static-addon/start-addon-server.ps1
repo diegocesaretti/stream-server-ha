356c8a5b9d95a93f1b476c9c48eadbc69196e470
diegocesaretti <47791066+diegocesaretti@users.noreply.github.com>
2026-07-17 22:26:01 -0300

    a

--- examples/static-addon/start-addon-server.ps1
+++
@@ -1,4 +0,0 @@
-Set-Location $PSScriptRoot
-Write-Host "Add-on disponible en http://IP_DE_ESTA_PC:7000/manifest.json"
-Write-Host "Para cerrar, presiona Ctrl+C."
-py -m http.server 7000 --bind 0.0.0.0
